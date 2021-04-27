4f1dd7408d0e0bd82195eb728abf6cb7d47970a4
fieryheart <fiery_heart@163.com>
2021-04-27 22:23:51 +0800

    shaders.h

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <unistd.h>
-#include "qgl.h"
+#include "shaders.h"
 
 Model *model = NULL;
 const int width  = 800;
@@ -18,223 +18,6 @@ struct Log {
     bool flag;
     std::string prefix;
     Log(bool _flag, std::string _prefix) : flag(_flag), prefix(_prefix){}
-};
-
-class GouraudShader : public Shader {
-public:
-    Vec3f varying_intensity;
-    Matrix mat_model, mat_view, mat_project, mat_viewport;
-    Matrix mat_transform;
-    Vec3f light;
-
-    void computeTranform() {
-        mat_transform = mat_viewport*mat_project*mat_view*mat_model;    
-    }
-
-    virtual Vec4f vertex(int nthface, int nthvert) {
-        varying_intensity[nthvert] = std::clamp(model->norm(nthface, nthvert)*light, 0.f, 1.f);
-        Vec4f gl_vertex = model->vert(nthface, nthvert);
-        gl_vertex = mat_transform*gl_vertex;
-        // std::cout << gl_vertex << std::endl;s
-        return  gl_vertex;
-    }
-    virtual bool fragment(Vec3f bar, TGAColor &color) {
-        float intensity = varying_intensity*bar;
-        color = TGAColor(255, 255, 255)*intensity;
-        return false;
-    }
-};
-
-// Phong Shader with no shadow
-class PhongShader : public Shader {
-public:
-    // 变换矩阵
-    Matrix mat_model, mat_view, mat_project, mat_viewport;
-    Matrix mat_transform;
-    Matrix mat_normal, mat_normal_it;
-    Vec3f light;
-
-    Vec4f gl_vertex[3];
-    Vec3f tex_uv[3];
-
-    void computeTranform() {
-        mat_transform = mat_viewport*mat_project*mat_view*mat_model;
-        mat_normal = mat_project*mat_view*mat_model;
-        mat_normal_it = mat_normal.inverse().transpose();
-    }
-
-    virtual Vec4f vertex(int nthface, int nthvert) {
-        gl_vertex[nthvert] = model->vert(nthface, nthvert);
-        gl_vertex[nthvert] = mat_transform*gl_vertex[nthvert];
-        tex_uv[nthvert] = model->texCoord(nthface, nthvert);
-        return  gl_vertex[nthvert]/gl_vertex[nthvert][3];
-    }
-    virtual bool fragment(Vec3f bar, TGAColor &color) {
-        Vec3f uv = tex_uv[0]*bar[0]+tex_uv[1]*bar[1]+tex_uv[2]*bar[2];
-        Vec4f n(model->normal(uv),0.f);
-        n = (mat_normal_it*n).normalize();
-        Vec4f l(light, 0.f);
-        l = (mat_normal*l).normalize();
-
-        Vec4f r = (n*(n*l*2.f) - l).normalize();   // reflected light
-
-        float spec = std::pow(std::max(r.z, 0.0f), model->specular(uv));
-        float diff = std::max(0.f, n*l);
-
-        TGAColor c = model->diffuse(uv);
-        
-        for (int i=0; i<3; i++) {
-            // 5. : 环境光部分
-            // 1. : 漫反射部分
-            // .6 : 高光部分
-            color[i] = std::min<float>(5. + c[i]*(1.*diff + .6*spec), 255);
-        }
-
-        return false;
-    }
-};
-
-// Depth Shader with custom camera
-class DepthShader : public Shader {
-public:
-    // 变换矩阵
-    Matrix mat_model, mat_view, mat_project, mat_viewport;
-    Matrix mat_transform, mat_transform_it;
-
-    // 顶点相关数据
-    float gl_depth[3];
-
-    void computeTranform() {
-        mat_transform = mat_viewport*mat_project*mat_view*mat_model;
-    }
-
-    virtual Vec4f vertex(int nthface, int nthvert) { 
-        Vec4f gl_vertex = model->vert(nthface, nthvert);
-        gl_vertex = mat_transform*gl_vertex;
-        gl_vertex = gl_vertex/gl_vertex[3];
-        gl_depth[nthvert] = gl_vertex[2];
-        return gl_vertex;
-    }
-
-    virtual bool fragment(Vec3f bar, TGAColor &color) {
-        int d = (int)(bar[0]*gl_depth[0]+bar[1]*gl_depth[1]+bar[2]*gl_depth[2]);
-        color = TGAColor(d, d, d)*255.0f;
-        return false;
-    }
-};
-
-// Phong Shader with shadow
-class ShadowShader : public Shader {
-public:
-    Matrix mat_model, mat_view, mat_project, mat_viewport;
-    Matrix mat_transform;
-    Matrix mat_normal, mat_normal_it;
-    Matrix mat_transform_depth;
-    Vec3f light;
-    TGAImage *depthmap;
-
-    Vec4f vertexes[3];
-    Vec4f gl_vertexes[3];
-    Vec3f tex_uv[3];
-
-    void computeTranform() {
-        mat_transform = mat_viewport*mat_project*mat_view*mat_model;
-        mat_normal = mat_project*mat_view*mat_model;
-        mat_normal_it = mat_normal.inverse().transpose();
-    }
-
-    virtual Vec4f vertex(int nthface, int nthvert) {
-        vertexes[nthvert] = model->vert(nthface, nthvert);
-        gl_vertexes[nthvert] = mat_transform*vertexes[nthvert];
-        tex_uv[nthvert] = model->texCoord(nthface, nthvert);
-        return  gl_vertexes[nthvert]/gl_vertexes[nthvert][3];
-    }
-
-    virtual bool fragment(Vec3f bar, TGAColor &color) {
-        // 纹理坐标
-        Vec3f uv = tex_uv[0]*bar[0]+tex_uv[1]*bar[1]+tex_uv[2]*bar[2];
-        Vec4f n(model->normal(uv),0.f);
-        n = (mat_normal_it*n).normalize();
-        Vec4f l(light, 0.f);
-        l = (mat_normal*l).normalize();
-
-        Vec4f r = (n*(n*l*2.f) - l).normalize();   // reflected light
-
-        float spec = std::pow(std::max(r.z, 0.0f), model->specular(uv));
-        float diff = std::max(0.f, n*l);
-
-        // 漫反射
-        TGAColor c = model->diffuse(uv);
-
-        // 阴影
-        Vec4f depth_vectex = vertexes[0]*bar[0]+vertexes[1]*bar[1]+vertexes[2]*bar[2];
-        depth_vectex = mat_transform_depth*depth_vectex;
-        depth_vectex = depth_vectex / depth_vectex[3];
-        int d = depthmap->get(int(depth_vectex[0]+.5f), int(depth_vectex[1]+.5f))[0];
-        float shadow = 1.f;
-        int bias = 1;
-        // std::cout << d << std::endl;
-        if (depth_vectex[2] < d-bias) {
-            // std::cout << shadow << std::endl;
-            shadow = .3f;
-        }
-        
-        // Phong + Shadow
-        for (int i=0; i<3; i++) {
-            // 5. : 环境光部分
-            // 1. : 漫反射部分
-            // .6 : 高光部分
-            color[i] = std::min<float>(5. + c[i]*shadow*(1.*diff + .6*spec), 255);
-        }
-
-        return false;
-    }
-};
-
-// AO Shader
-class OcclusionShader : public Shader {
-public:
-    Matrix mat_model, mat_view, mat_viewport;
-    Matrix mat_transform;
-    Matrix mat_transform_depth;
-
-    TGAImage *depthmap;
-    TGAImage *occlusionmap;
-
-    Vec4f vertexes[3];
-    Vec2f tex_uv[3];
-
-    void computeTranform() {
-        mat_transform = mat_viewport*mat_view*mat_model;
-        // std::cout << mat_transform << std::endl;
-    }
-
-    virtual Vec4f vertex(int nthface, int nthvert) { 
-        vertexes[nthvert] = model->vert(nthface, nthvert);
-        vertexes[nthvert] = mat_transform*vertexes[nthvert];
-        Vec3f tex = model->texCoord(nthface, nthvert);
-        tex_uv[nthvert].x = tex.x * occlusionmap->get_width();
-        tex_uv[nthvert].y = tex.y * occlusionmap->get_height();
-        return vertexes[nthvert]/vertexes[nthvert][3];
-    }
-
-    virtual bool fragment(Vec3f bar, TGAColor &color) {
-        // 纹理坐标
-        Vec2f uv = tex_uv[0]*bar[0]+tex_uv[1]*bar[1]+tex_uv[2]*bar[2];
-        
-        // gl坐标
-        Vec4f vertex = vertexes[0]*bar[0]+vertexes[1]*bar[1]+vertexes[2]*bar[2];
-
-        // std::cout << "depthmap.z: " << (float)((depthmap->get((int)(vertex.x+.5f), (int)(vertex.y+.5f)))[0]) << std::endl; 
-        // std::cout << "vertex.z: " << vertex.z << std::endl;
-
-        if (std::abs((depthmap->get((int)(vertex.x+.5f), (int)(vertex.y+.5f)))[0]-vertex.z) < 1) {
-            occlusionmap->set((int)(uv.x+.5f), (int)(uv.y+.5f), 255);
-        }
-        color = TGAColor(255, 0, 0);
-        return false;
-
-    }
 };
 
 // 在指定半径的半球上产生随机点
@@ -279,6 +62,7 @@ void DepthShading() {
     Vec3f up(0, 1, 0);
 
     DepthShader depthShader;
+    depthShader.model = model;
     depthShader.mat_model = GetModelMatrix();
     depthShader.mat_view = GetViewMatrix(light, origin, up);
     depthShader.mat_project = Matrix::identity(4);
@@ -304,6 +88,7 @@ void PhongShadingNoShadow() {
     Vec3f up(0, 1, 0);
 
     PhongShader phongShader;
+    phongShader.model = model;
     phongShader.light = light;
     phongShader.mat_model = GetModelMatrix();
     phongShader.mat_view = GetViewMatrix(camera, origin, up);
@@ -335,6 +120,7 @@ void PhongShadingHardShadow() {
 
     // Depth Shading
     DepthShader depthShader;
+    depthShader.model = model;
     depthShader.mat_model = GetModelMatrix();
     depthShader.mat_view = GetViewMatrix(light, origin, up);
     depthShader.mat_project = Matrix::identity(4);
@@ -344,6 +130,7 @@ void PhongShadingHardShadow() {
 
     // Shadow Shading
     ShadowShader shadowShader;
+    shadowShader.model = model;
     shadowShader.light = light;
     shadowShader.mat_model = GetModelMatrix();
     shadowShader.mat_view = GetViewMatrix(camera, origin, up);
@@ -400,6 +187,7 @@ void AmbientOcclusion() {
         // zbuffer.clear();
         std::fill(zbuffer, zbuffer+width*height, -std::numeric_limits<float>::max());
         DepthShader depthShader;
+        depthShader.model = model;
         depthShader.mat_model = GetModelMatrix();
         depthShader.mat_view = GetViewMatrix(light, origin, up);
         depthShader.mat_project = Matrix::identity(4);
@@ -411,6 +199,7 @@ void AmbientOcclusion() {
         // The second pass
         // 在occlusion上着点
         OcclusionShader occlusionShader;
+        occlusionShader.model = model;
         occlusionShader.mat_model = GetModelMatrix();
         occlusionShader.mat_view = GetViewMatrix(light, origin, up);
         occlusionShader.mat_viewport = GetViewportMatrix(width/8, height/8, width*3/4, width*3/4, depth);
@@ -472,20 +261,6 @@ float _get_max_slope(float *zbuffer, Vec2f pos, Vec2f dir) {
     return max_slope;
 }
 
-// float _get_max_slope(float *zbuffer, Vec2f p, Vec2f dir) {
-//     float maxangle = 0;
-//     for (float t=0.; t<1000.; t+=1.) {
-//         Vec2f cur = p + dir*t;
-//         if (cur.x>=width || cur.y>=height || cur.x<0 || cur.y<0) return maxangle;
-
-//         float distance = (p-cur).norm();
-//         if (distance < 1.f) continue;
-//         float elevation = zbuffer[int(cur.x)+int(cur.y)*width]-zbuffer[int(p.x)+int(p.y)*width];
-//         maxangle = std::max(maxangle, atanf(elevation/distance));
-//     }
-//     return maxangle;
-// }
-
 // Implementation: SSAO, Screen Space Ambient Occlusion
 // zbuffer 不能使用int存储，不然会出现明显的分割纹路
 void ScreenSpaceAmbientOcclusion() {
@@ -500,6 +275,7 @@ void ScreenSpaceAmbientOcclusion() {
     Vec3f up(0, 1, 0);
 
     DepthShader depthShader;
+    depthShader.model = model;
     depthShader.mat_model = GetModelMatrix();
     depthShader.mat_view = GetViewMatrix(camera, origin, up);
     // depthShader.mat_project = Matrix::identity(4);
---
+++ shaders.h
@@ -0,0 +1,223 @@
+#include "qgl.h"
+
+class GouraudShader : public Shader {
+public:
+    Model *model;
+    Vec3f varying_intensity;
+    Matrix mat_model, mat_view, mat_project, mat_viewport;
+    Matrix mat_transform;
+    Vec3f light;
+
+    void computeTranform() {
+        mat_transform = mat_viewport*mat_project*mat_view*mat_model;    
+    }
+
+    virtual Vec4f vertex(int nthface, int nthvert) {
+        varying_intensity[nthvert] = std::clamp(model->norm(nthface, nthvert)*light, 0.f, 1.f);
+        Vec4f gl_vertex = model->vert(nthface, nthvert);
+        gl_vertex = mat_transform*gl_vertex;
+        // std::cout << gl_vertex << std::endl;s
+        return  gl_vertex;
+    }
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        float intensity = varying_intensity*bar;
+        color = TGAColor(255, 255, 255)*intensity;
+        return false;
+    }
+};
+
+// Phong Shader with no shadow
+class PhongShader : public Shader {
+public:
+    Model *model;
+    // 变换矩阵
+    Matrix mat_model, mat_view, mat_project, mat_viewport;
+    Matrix mat_transform;
+    Matrix mat_normal, mat_normal_it;
+    Vec3f light;
+
+    Vec4f gl_vertex[3];
+    Vec3f tex_uv[3];
+
+    void computeTranform() {
+        mat_transform = mat_viewport*mat_project*mat_view*mat_model;
+        mat_normal = mat_project*mat_view*mat_model;
+        mat_normal_it = mat_normal.inverse().transpose();
+    }
+
+    virtual Vec4f vertex(int nthface, int nthvert) {
+        gl_vertex[nthvert] = model->vert(nthface, nthvert);
+        gl_vertex[nthvert] = mat_transform*gl_vertex[nthvert];
+        tex_uv[nthvert] = model->texCoord(nthface, nthvert);
+        return  gl_vertex[nthvert]/gl_vertex[nthvert][3];
+    }
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        Vec3f uv = tex_uv[0]*bar[0]+tex_uv[1]*bar[1]+tex_uv[2]*bar[2];
+        Vec4f n(model->normal(uv),0.f);
+        n = (mat_normal_it*n).normalize();
+        Vec4f l(light, 0.f);
+        l = (mat_normal*l).normalize();
+
+        Vec4f r = (n*(n*l*2.f) - l).normalize();   // reflected light
+
+        float spec = std::pow(std::max(r.z, 0.0f), model->specular(uv));
+        float diff = std::max(0.f, n*l);
+
+        TGAColor c = model->diffuse(uv);
+        
+        for (int i=0; i<3; i++) {
+            // 5. : 环境光部分
+            // 1. : 漫反射部分
+            // .6 : 高光部分
+            color[i] = std::min<float>(5. + c[i]*(1.*diff + .6*spec), 255);
+        }
+
+        return false;
+    }
+};
+
+// Depth Shader with custom camera
+class DepthShader : public Shader {
+public:
+    Model *model;
+    // 变换矩阵
+    Matrix mat_model, mat_view, mat_project, mat_viewport;
+    Matrix mat_transform, mat_transform_it;
+
+    // 顶点相关数据
+    float gl_depth[3];
+
+    void computeTranform() {
+        mat_transform = mat_viewport*mat_project*mat_view*mat_model;
+    }
+
+    virtual Vec4f vertex(int nthface, int nthvert) { 
+        Vec4f gl_vertex = model->vert(nthface, nthvert);
+        gl_vertex = mat_transform*gl_vertex;
+        gl_vertex = gl_vertex/gl_vertex[3];
+        gl_depth[nthvert] = gl_vertex[2];
+        return gl_vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        int d = (int)(bar[0]*gl_depth[0]+bar[1]*gl_depth[1]+bar[2]*gl_depth[2]);
+        color = TGAColor(d, d, d)*255.0f;
+        return false;
+    }
+};
+
+// Phong Shader with shadow
+class ShadowShader : public Shader {
+public:
+    Model *model;
+    Matrix mat_model, mat_view, mat_project, mat_viewport;
+    Matrix mat_transform;
+    Matrix mat_normal, mat_normal_it;
+    Matrix mat_transform_depth;
+    Vec3f light;
+    TGAImage *depthmap;
+
+    Vec4f vertexes[3];
+    Vec4f gl_vertexes[3];
+    Vec3f tex_uv[3];
+
+    void computeTranform() {
+        mat_transform = mat_viewport*mat_project*mat_view*mat_model;
+        mat_normal = mat_project*mat_view*mat_model;
+        mat_normal_it = mat_normal.inverse().transpose();
+    }
+
+    virtual Vec4f vertex(int nthface, int nthvert) {
+        vertexes[nthvert] = model->vert(nthface, nthvert);
+        gl_vertexes[nthvert] = mat_transform*vertexes[nthvert];
+        tex_uv[nthvert] = model->texCoord(nthface, nthvert);
+        return  gl_vertexes[nthvert]/gl_vertexes[nthvert][3];
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        // 纹理坐标
+        Vec3f uv = tex_uv[0]*bar[0]+tex_uv[1]*bar[1]+tex_uv[2]*bar[2];
+        Vec4f n(model->normal(uv),0.f);
+        n = (mat_normal_it*n).normalize();
+        Vec4f l(light, 0.f);
+        l = (mat_normal*l).normalize();
+
+        Vec4f r = (n*(n*l*2.f) - l).normalize();   // reflected light
+
+        float spec = std::pow(std::max(r.z, 0.0f), model->specular(uv));
+        float diff = std::max(0.f, n*l);
+
+        // 漫反射
+        TGAColor c = model->diffuse(uv);
+
+        // 阴影
+        Vec4f depth_vectex = vertexes[0]*bar[0]+vertexes[1]*bar[1]+vertexes[2]*bar[2];
+        depth_vectex = mat_transform_depth*depth_vectex;
+        depth_vectex = depth_vectex / depth_vectex[3];
+        int d = depthmap->get(int(depth_vectex[0]+.5f), int(depth_vectex[1]+.5f))[0];
+        float shadow = 1.f;
+        int bias = 1;
+        // std::cout << d << std::endl;
+        if (depth_vectex[2] < d-bias) {
+            // std::cout << shadow << std::endl;
+            shadow = .3f;
+        }
+        
+        // Phong + Shadow
+        for (int i=0; i<3; i++) {
+            // 5. : 环境光部分
+            // 1. : 漫反射部分
+            // .6 : 高光部分
+            color[i] = std::min<float>(5. + c[i]*shadow*(1.*diff + .6*spec), 255);
+        }
+
+        return false;
+    }
+};
+
+// AO Shader
+class OcclusionShader : public Shader {
+public:
+    Model *model;
+    Matrix mat_model, mat_view, mat_viewport;
+    Matrix mat_transform;
+    Matrix mat_transform_depth;
+
+    TGAImage *depthmap;
+    TGAImage *occlusionmap;
+
+    Vec4f vertexes[3];
+    Vec2f tex_uv[3];
+
+    void computeTranform() {
+        mat_transform = mat_viewport*mat_view*mat_model;
+        // std::cout << mat_transform << std::endl;
+    }
+
+    virtual Vec4f vertex(int nthface, int nthvert) { 
+        vertexes[nthvert] = model->vert(nthface, nthvert);
+        vertexes[nthvert] = mat_transform*vertexes[nthvert];
+        Vec3f tex = model->texCoord(nthface, nthvert);
+        tex_uv[nthvert].x = tex.x * occlusionmap->get_width();
+        tex_uv[nthvert].y = tex.y * occlusionmap->get_height();
+        return vertexes[nthvert]/vertexes[nthvert][3];
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        // 纹理坐标
+        Vec2f uv = tex_uv[0]*bar[0]+tex_uv[1]*bar[1]+tex_uv[2]*bar[2];
+        
+        // gl坐标
+        Vec4f vertex = vertexes[0]*bar[0]+vertexes[1]*bar[1]+vertexes[2]*bar[2];
+
+        // std::cout << "depthmap.z: " << (float)((depthmap->get((int)(vertex.x+.5f), (int)(vertex.y+.5f)))[0]) << std::endl; 
+        // std::cout << "vertex.z: " << vertex.z << std::endl;
+
+        if (std::abs((depthmap->get((int)(vertex.x+.5f), (int)(vertex.y+.5f)))[0]-vertex.z) < 1) {
+            occlusionmap->set((int)(uv.x+.5f), (int)(uv.y+.5f), 255);
+        }
+        color = TGAColor(255, 0, 0);
+        return false;
+
+    }
+};
